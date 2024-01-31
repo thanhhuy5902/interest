5a9abb9831d3648a0a7ce956d30096e05df61a95
Duy Lu Hoang <duy.lh@itss.edu.vn>
2024-01-31 15:17:34 +0700

    khoi tao project, cai dat firebase, taigaUI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,36 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'itss-imago-0000',
+          appId: '1:1098187958856:web:931b5d503852e1c9a1867d',
+          storageBucket: 'itss-imago-0000.appspot.com',
+          apiKey: 'AIzaSyAJ93BuFGs7gOJe9kudLYvAn4-Fp6Q936M',
+          authDomain: 'itss-imago-0000.firebaseapp.com',
+          messagingSenderId: '1098187958856',
+          measurementId: 'G-7TVCQGP8RS',
+        }),
+      ),
+      TuiRootModule,
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
