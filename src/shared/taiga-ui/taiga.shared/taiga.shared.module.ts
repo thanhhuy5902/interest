3cd94f9d6835dd6a5db166b9d0c7631f1413636b
bqtruong2000 <bqtruong2021@gmail.com>
2024-02-01 10:56:15 +0700

    setup library

---
+++ src/shared/taiga-ui/taiga.shared/taiga.shared.module.ts
@@ -0,0 +1,59 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+  TuiAvatarModule,
+  TuiBadgedContentModule,
+  TuiBadgeModule,
+  TuiBreadcrumbsModule,
+  TuiCarouselModule,
+  TuiCheckboxModule,
+  TuiElasticContainerModule,
+  TuiFilterModule,
+  TuiInputModule,
+  TuiIslandModule,
+  TuiMultiSelectModule,
+  TuiProgressModule,
+  TuiSelectModule,
+  TuiStepperModule,
+  TuiTabsModule,
+  TuiTagModule,
+  TuiToggleModule
+} from '@taiga-ui/kit';
+import {
+  TuiButtonModule,
+  TuiDialogModule,
+  TuiDropdownModule,
+  TuiErrorModule,
+  TuiExpandModule,
+  TuiLoaderModule,
+  TuiNotificationModule,
+  TuiScrollbarModule,
+  TuiSvgModule,
+  TuiTooltipModule
+} from '@taiga-ui/core';
+import {TuiBlockStatusModule} from "@taiga-ui/layout";
+
+const TaigaModules = [TuiTabsModule,
+  TuiSvgModule,
+  TuiInputModule,
+  TuiAvatarModule,
+  TuiBadgeModule,
+  TuiBadgedContentModule,
+  TuiLoaderModule,
+  TuiCarouselModule,
+  TuiIslandModule, TuiCheckboxModule, TuiDialogModule, TuiDropdownModule, TuiElasticContainerModule, TuiToggleModule, TuiErrorModule, TuiExpandModule, TuiFilterModule, TuiMultiSelectModule, TuiSelectModule, TuiTooltipModule, TuiNotificationModule, TuiProgressModule, TuiScrollbarModule, TuiTagModule, TuiBlockStatusModule, TuiButtonModule, TuiTabsModule, TuiBreadcrumbsModule, TuiStepperModule];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...TaigaModules,
+  ],
+  exports: [
+    ...TaigaModules,
+  ],
+})
+
+export class TaigaSharedModule {
+}
