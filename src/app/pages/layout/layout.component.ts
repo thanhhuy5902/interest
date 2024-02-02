f9280eb8fc35dd5f9b38794fe47535920151c6a3
bqtruong2000 <bqtruong2021@gmail.com>
2024-02-02 09:40:27 +0700

    add router-outlet

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    RouterOutlet
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
