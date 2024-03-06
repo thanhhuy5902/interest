37f18d16f3cb83ba2695e6dccb7ea87a4c8eb564
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2024-03-06 19:41:40 +0700

    .

--- src/app/pages/layout/home/home.component.ts
+++ src/app/pages/layout/home/home.component.ts
@@ -9,4 +9,11 @@ import { ShareModule } from '../../../shared/share.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  bannerImage =
+    'https://cdn.tgdd.vn/Files/2020/08/20/1281844/thit-bo-kobe-co-gi-ma-sao-lai-dat-do-den-nhu-vay-202008201518109704.jpg';
+  readonly avatarUrl = `https://taiga-ui.dev/assets/images/avatar.jpg`;
+  onClick(event: MouseEvent): void {
+    console.info('click ', this.avatarUrl, event);
+  }
+}
